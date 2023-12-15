265288da487e6c11763e513c76fc2b12f4c7a1c0
VuVietDuy <vivetduy1234@gmail.com>
2023-12-15 23:45:21 +0700

    fix

--- modules/user/LearningPage/Layout2/index.tsx
+++ modules/user/LearningPage/Layout2/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, Route, Routes } from "react-router-dom";
+// import { NavLink, Route, Routes } from "react-router-dom";
 import AboutOption from "./AboutOption";
 import ProjectsResources from "./ProjectsResources";
 import Discussions from "./Discussions";
@@ -11,13 +11,13 @@ function Layout2() {
     <div className="layout2-learningpage">
       <div className="learning-nav">
         <div className="learning-nav-contain">
-          <NavLink to={"about"}><div className="learning-nav-btn">About</div></NavLink>
+          {/* <NavLink to={"about"}><div className="learning-nav-btn">About</div></NavLink>
           <NavLink to={"projects_resources"}><div className="learning-nav-btn">Projects & Resources</div></NavLink>
           <NavLink to={"discussions"}><div className="learning-nav-btn">Discussions</div></NavLink>
-          <NavLink to={"review"}><div className="learning-nav-btn">Review</div></NavLink>
+          <NavLink to={"review"}><div className="learning-nav-btn">Review</div></NavLink> */}
         </div>
       </div>
-      <Routes>
+      {/* <Routes>
         <Route path="about" element={<AboutOption></AboutOption>}></Route>
         <Route
           path="projects_resources"
@@ -25,7 +25,7 @@ function Layout2() {
         ></Route>
         <Route path="discussions" element={<Discussions></Discussions>}></Route>
         <Route path="review" element={<Review></Review>}></Route>
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
