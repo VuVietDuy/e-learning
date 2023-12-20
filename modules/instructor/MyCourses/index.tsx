e5bfd084f9b3affeb07bd3187cd0bf5a68cfe17b
VuVietDuy <vuvietduy1234@gmail.com>
2023-12-20 14:07:42 +0700

    update

--- modules/instructor/MyCourses/index.tsx
+++ modules/instructor/MyCourses/index.tsx
@@ -1,34 +1,40 @@
-'use client'
-import React from 'react'
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { Button } from 'antd';
-import { DeleteFilled } from '@ant-design/icons';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "antd";
+import { DeleteFilled } from "@ant-design/icons";
 
-import './style.scss'
+import "./style.scss";
 
 export function MyCourses() {
-
-  const router = useRouter()
-    function editCourse() {
-      router.push("/instructor/detail_course")
-    }
+  const router = useRouter();
+  const editCourse = () => {
+    router.push("/instructor/detail_course");
+  }
   return (
-    <div className='default_container my_courses_container'>
-      <div className='header d_flex justify_content_between'>
+    <div className="default_container my_courses_container">
+      <div className="header d_flex justify_content_between">
         <h2>My Courses</h2>
-        <Button >New course</Button>
+        <Button>New course</Button>
       </div>
-      <div className='courses d_flex justify_content_between align_item_center'>
-        <div className='d_flex justify_content_between align_item_center'>
-          <Image src="/images/instructor/course_thumpail.png" alt='' width={232} height={144}></Image>
-          <h3 className='ms_4'>Course title</h3>
+      <div className="courses d_flex justify_content_between align_item_center">
+        <div className="d_flex justify_content_between align_item_center">
+          <Image
+            src="/images/instructor/course_thumpail.png"
+            alt=""
+            width={232}
+            height={144}
+          ></Image>
+          <h3 className="ms_4">Course title</h3>
         </div>
         <div>
           <Button onClick={editCourse}>Edit</Button>
-          <Button className='ms_4'><DeleteFilled /></Button>
+          <Button className="ms_4">
+            <DeleteFilled />
+          </Button>
         </div>
       </div>
     </div>
-  )
+  );
 }
