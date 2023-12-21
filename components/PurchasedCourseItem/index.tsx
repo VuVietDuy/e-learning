598410c671eea08cde69ac7e484094abe83bd79f
VuVietDuy <vuvietduy1234@gmail.com>
2023-12-21 21:17:56 +0700

    update

--- components/PurchasedCourseItem/index.tsx
+++ components/PurchasedCourseItem/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./index.scss";
 
 export default function CoursePurchased(props: any): JSX.Element {
+
   const { src, title, instructor, time, amount } = props;
+
   return (
     <div className="purchase_course">
       <div className="course__3 mt24">
@@ -15,11 +17,10 @@ export default function CoursePurchased(props: any): JSX.Element {
             <div className="course-3-des-1">{title}</div>
             <div className="course-3-des-2-1">
               <div className=" fz-20">{instructor}</div>
-
               <div className="des-2-3 d_flex des-2-3-margin">
-                <div className=" fz-20 ml5">{time}</div>
+                <div className=" fz-20 ml5"><span>{time}</span></div>
                 <div className="dot"></div>
-                <div className=" fz-20">{amount} Students</div>
+                <div className=" fz-20"><span>{amount} Students</span></div>
               </div>
               <div>
                 <b>Purchased date</b> 02/08/2023
